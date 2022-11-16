845a2609615047100782b49b568f2f58ca9fdbab
Dmitry Shurshilin <dmitry.shurshilin@gmail.com>
2022-11-16 17:47:07 +0600

    Update files

--- js/modules/main.js
+++ js/modules/main.js
@@ -1,3 +1,11 @@
 import o from './modules.js'
 
 console.log(o)
+o.name = 'Update object name'
+
+document.getElementById('Button').addEventListener('click', () => {
+    const script = document.createElement('script')
+    script.src = 'second.js'
+    script.type = 'module'
+    document.appendChild(script)
+})
---
+++ js/modules/second.js
@@ -0,0 +1,7 @@
+import o from './modules.js'
+
+console.log(o)
+
+setTimeout(() => {
+    console.log(o)
+}, 5_000)
